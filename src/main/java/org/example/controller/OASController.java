b0b3adfc5f1c6d4f5787e8bc2904eee7d46249df
Palamarchuk Ivan <vanavana978@gmail.com>
2024-02-25 13:53:10 +0200

    added swagger support

---
+++ src/main/java/org/example/controller/OASController.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OASController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:swagger-ui.html";
+    }
+
+}
